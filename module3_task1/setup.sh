014b7ac9fff4030875f4b93d620d37f5ec0c4d62
BesterGG <jol.alvarezc@gmail.com>
2023-01-08 23:38:29 -0300

    SUDO

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,4 +1,5 @@
-sudo apt-get update && sudo apt-get install -y make wget
+#!/bin/bash
+sudo apt-get update && sudo apt-get install -y make wget build-essential
 wget https://github.com/gohugoio/hugo/releases/download/v0.79.0/hugo_0.79.0_Linux-64bit.deb
 dpkg -i hugo_0.79.0_Linux-64bit.deb
 make build
